2dc3cc16c01c6577a7764fe0cf1d4c6ff0103d3b
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-04-02 15:54:24 -0300

    Express

--- 7_template_engine/1_instalando_handlebars/index.js
+++ 7_template_engine/1_instalando_handlebars/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const exphbs = require("express-handlebars").create({ /* configurações */ });
+// const exphbs = require("express-handlebars").create({ /* configurações */ });
+const exphbs = require("express-handlebars");
 
 const app = express();
 
-app.engine('handlebars', exphbs.engine);
+const hbs = exphbs.create({
+    partialsDir: ['views/partials'],
+})
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
 
 app.get('/dashboard', (req, res) => {
 
@@ -26,6 +33,33 @@ app.get('/', (req, res) => {
     res.render("home", { user: user, nacionalidade: "Brasileiro", auth: auth })
 });
 
+app.get('/blog', (rq, res) => {
+    const posts = [
+        {
+            title: "Aprender Node.js",
+            category: "Javascript",
+            body: "Test",
+            comments: 4
+        },
+        {
+            title: "Aprender js",
+            category: "Javascript",
+            body: "Test",
+            comments: 4
+        },
+        {
+            title: "Aprender Gabriel",
+            category: "Javascript",
+            body: "Test",
+            comments: 4
+        },
+    ]
+
+    res.render('blog', { posts })
+})
+
 app.listen(3000, () => {
     console.log("App rodando!");
 });
+
+
