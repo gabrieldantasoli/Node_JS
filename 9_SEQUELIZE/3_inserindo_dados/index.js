9f9bef2246b01f23591165001374d6c349845c97
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-04-21 20:27:29 -0300

    SEQUELIZE - inserindo dados

---
+++ 9_SEQUELIZE/3_inserindo_dados/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const conn = require('./db/conn')
+
+const User = require('./models/User');
+
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true
+    }),
+);
+app.use(express.json());
+
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+
+app.get('/users/create', (req, res) => {
+    res.render('adduser');
+});
+
+app.post('/users/create', async (req, res) => {
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+    let newsletter = req.body.newsletter;
+
+    if (newsletter == 'on') {
+        newsletter = true;
+    } else {
+        newsletter = false;
+    }
+
+    try {
+        await User.create({name, occupation, newsletter});
+        res.redirect('/');
+    } catch(err) {
+        console.log(err);
+        res.redirect('/users/create');
+    }
+});
+
+
+
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+
+conn.sync()
+.then(() => {
+    app.listen(3000);
+})
+.catch((err) => console.log(err));
+
