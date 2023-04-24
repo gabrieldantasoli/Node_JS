58cbf1b45dc4f1856277064e1d7b6c90970758c6
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-04-24 15:57:05 -0300

    SEQUELIZE - destroy and where

---
+++ 9_SEQUELIZE/6_removendo_dados/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('nodesequelize', 'root', '201000267165', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+/*try {
+  sequelize.authenticate();
+  console.log("Conectado ao Sequelize !");
+} catch (err) {
+  console.log("Não foi possível conectar : ", err);
+}*/
+
+module.exports = sequelize;
