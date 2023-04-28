29b8324c027b018b6ac4c0a405028a4cfac7cbfc
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-04-28 14:53:45 -0300

    MVC structure - Saving Task into Database

--- 10_MVC/1_estrutura/controllers/TaskController.js
+++ 10_MVC/1_estrutura/controllers/TaskController.js
@@ -5,6 +5,18 @@ module.exports = class TaskContoller {
         res.render('tasks/create');
     }
 
+    static async createTaskSave(req, res) {
+        const task = {
+            title: req.body.title,
+            decription: req.body.decription,
+            done: false
+        }
+
+        await Task.create(task);
+
+        res.redirect('/tasks');
+    }
+
     static showTasks(req, res) {
         res.render('tasks/all');
     }
--- 10_MVC/1_estrutura/routes/taskRoutes.js
+++ 10_MVC/1_estrutura/routes/taskRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const TaskContoller = require('../controllers/TaskController');
 
 router.get('/add', TaskContoller.createTask);
+router.post('/add', TaskContoller.createTaskSave)
 router.get('/', TaskContoller.showTasks);
 
 module.exports = router;
