abe13f1ccbbb9dea29ec7e350b88b5649ca05565
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-05-23 10:42:04 -0300

    Initializing MongoDB

---
+++ 12_MONGODB/1_instalacao/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const conn = require("./db/conn");
+
+app.use(
+    express.urlencoded({
+        extended: true
+    }),
+);
+
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+
+console.log("ok");
+
+app.listen(3000);
