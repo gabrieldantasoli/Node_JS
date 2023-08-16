9786594b2caa4da752657fe2260ce0c65a8fe69c
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-08-16 19:58:54 -0300

    Tipos de dados

--- Learning_TYPESCRIPT/3_TIPOS/index.js
+++ Learning_TYPESCRIPT/3_TIPOS/index.js
@@ -12,3 +12,30 @@ const arr1 = [1, "Teste", true, []];
 arr1.push([1, 2, 3, 4, 5]);
 console.log(arr1);
 //--------------------------------------------------------------
+// 3 - funções com parametros tipados
+function soma(a, b) {
+    let soma = a + b;
+    console.log(`A soma de ${a} e ${b} é = `, soma.toFixed(2));
+}
+soma(2, 9.999);
+//--------------------------------------------------------------
+// 4 - retorno da função
+function greeting(name) {
+    return `Olá, ${name}!`;
+}
+console.log(greeting("Gabriel Dantas"));
+//--------------------------------------------------------------
+// 5 - função anonimas
+setTimeout(() => {
+    const salary = 1000;
+    //console.log(salary);
+}, 2000);
+//--------------------------------------------------------------
+// 6 - tipos de objetos
+function passCoordinates(coord) {
+    console.log("X coordinate: " + coord.x);
+    console.log("Y coordinate: " + coord.y);
+}
+const objCoord = { x: 1900, y: 2100 };
+passCoordinates(objCoord);
+//--------------------------------------------------------------
--- Learning_TYPESCRIPT/3_TIPOS/index.ts
+++ Learning_TYPESCRIPT/3_TIPOS/index.ts
@@ -17,3 +17,40 @@ arr1.push([1,2,3,4,5]);
 
 console.log(arr1);
 //--------------------------------------------------------------
+
+// 3 - funções com parametros tipados
+function soma(a: number, b: number) {
+    let soma: number = a + b;
+    console.log(`A soma de ${a} e ${b} é = `, soma.toFixed(2));
+}
+
+soma(2, 9.999);
+//--------------------------------------------------------------
+
+// 4 - retorno da função
+function greeting(name: string) {
+    return `Olá, ${name}!`
+}
+
+console.log(greeting("Gabriel Dantas"));
+//--------------------------------------------------------------
+
+// 5 - função anonimas
+setTimeout(() => {
+    const salary: number = 1000;
+
+    //console.log(salary);
+}, 2000);
+//--------------------------------------------------------------
+
+// 6 - tipos de objetos
+function passCoordinates(coord: {x: number, y: number}) {
+    console.log("X coordinate: " + coord.x);
+    console.log("Y coordinate: " + coord.y);
+}
+
+const objCoord = {x: 1900, y: 2100}; 
+
+passCoordinates(objCoord);
+//--------------------------------------------------------------
+
