ae82ee0b3ff92c4348e149cb6e81af6645f6a236
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-09-02 19:51:13 -0300

    Learning Typescript

--- Learning_TYPESCRIPT/4_NARROWING/index.js
+++ Learning_TYPESCRIPT/4_NARROWING/index.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // 1 - type guard
 function sum(a, b) {
     if (typeof a === "string" && typeof b === "string") {
@@ -36,3 +51,51 @@ function operations(arr, operation) {
 operations([1, 2, 3, 4, 5], "SA");
 operations([1, 2, 3, 4, 5], "MA");
 operations([1, 2, 3, 4, 5], "EXP");
+// 3 - instance of
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
+var SuperUser = /** @class */ (function (_super) {
+    __extends(SuperUser, _super);
+    function SuperUser(name) {
+        return _super.call(this, name) || this;
+    }
+    return SuperUser;
+}(User));
+var jhon = new User("Jhon");
+var paul = new SuperUser("Paul");
+function userGreeting(user) {
+    if (user instanceof SuperUser) {
+        console.log("Olá, chefe!");
+    }
+    else if (user instanceof User) {
+        console.log("Olá, funcionário!");
+    }
+}
+userGreeting(jhon);
+userGreeting(paul);
+// 4 - operador in
+var Dog = /** @class */ (function () {
+    function Dog(name, raca) {
+        this.name = name;
+        if (raca) {
+            this.raca = raca;
+        }
+    }
+    return Dog;
+}());
+var turca = new Dog("Turca", "Labrador");
+var lili = new Dog("Lili");
+function showDogDetails(dog) {
+    if ('raca' in dog) {
+        console.log("O cachorro \u00E9 da ra\u00E7a ".concat(dog.raca));
+    }
+    else {
+        console.log("O cachorro n\u00E3o tem uma ra\u00E7a definida!");
+    }
+}
+showDogDetails(turca);
+showDogDetails(lili);
--- Learning_TYPESCRIPT/4_NARROWING/index.ts
+++ Learning_TYPESCRIPT/4_NARROWING/index.ts
@@ -35,3 +35,59 @@ function operations(arr: number[], operation?: string | undefined) {
 operations([1,2,3,4,5], "SA");
 operations([1,2,3,4,5], "MA");
 operations([1,2,3,4,5], "EXP");
+
+// 3 - instance of
+class User {
+    name
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+class SuperUser extends User {
+    constructor(name: string) {
+        super(name)
+    }
+}
+
+const jhon = new User("Jhon");
+const paul = new SuperUser("Paul");
+
+function userGreeting(user: object) {
+    if (user instanceof SuperUser) {
+        console.log("Olá, chefe!");
+    } else if (user instanceof User) {
+        console.log("Olá, funcionário!");
+    }
+}
+
+userGreeting(jhon);
+userGreeting(paul);
+
+// 4 - operador in
+class Dog {
+    name
+    raca
+
+    constructor(name: string, raca?: string) {
+        this.name = name;
+        if (raca) {
+            this.raca = raca;
+        }
+    }
+}
+
+const turca = new Dog("Turca","Labrador");
+const lili = new Dog("Lili");
+
+function showDogDetails(dog: Dog) {
+    if ('raca' in dog) {
+        console.log(`O cachorro é da raça ${dog.raca}`);
+    } else {
+        console.log(`O cachorro não tem uma raça definida!`);
+    }
+}
+
+showDogDetails(turca);
+showDogDetails(lili);
