89629a2e1554917fa849d5cb209f31dee4fc0fad
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-11-14 13:23:12 -0300

    Middleware

--- API/src/routes/user.routes.js
+++ API/src/routes/user.routes.js
@@ -4,6 +4,19 @@ import { create, getId, query1 } from "../controllers/UsersController.js";
 
 const routes = express.Router();
 
+function  myMiddleware(req, res, next) {
+    console.log("Você passou pelo Midldleware!");
+
+    if (!req.body.isAdmin) {
+        return res.json({ message: "User unauthorized!"});
+    }
+
+    next();
+}
+
+routes.use(myMiddleware);
+
+
 routes.get("/message/:id", getId)
 
 routes.get("/", query1);
